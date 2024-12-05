03d5a01496d264f92a01254f8464c349cef1e04f
AlexandreTheDwarf <alexandre-vandewiele@outlook.com>
2024-12-05 22:17:39 +0100

    need to solve a bug

--- public/script/main.js
+++ public/script/main.js
@@ -347,7 +347,7 @@ async function game() {
             playSound("./public/sound/hadouken.mp3");
             await sleep(5000); // Ajoute un délai de 1 seconde après chaque attaque du boss
         }
-    } while (heros.length > 0 || bossGame.vie > 0); // Continue tant que les héros sont vivants et que le boss n'est pas mort
+    } while (heros.length > 0 && bossGame.vie > 0); // Continue tant que les héros sont vivants et que le boss n'est pas mort
 }
 
 
